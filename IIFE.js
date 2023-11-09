4ec9a14e69815dea396036af392861d3b49bfaf3
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-11-09 23:56:20 +0530

    Update IIFE.js

--- IIFE.js
+++ IIFE.js
@@ -5,3 +5,24 @@
     }
   }
 })(0, 10);
+
+/*
+
+Difference Between the Normal Function and IIFE
+
+Normal Function: You define it first and then call it whenever you need to execute the code inside it. You can invoke it multiple times.
+
+function myFunction() {
+}
+myFunction();
+The function can be called at a later time, possibly multiple times
+
+
+IIFE: It is executed immediately after its creation, and it cannot be invoked again later. It runs exactly once—right after its definition.
+
+(function() {
+  
+})();
+  
+
+*/
